a72d3a82c8d33f130ba8621dda00a8ddac6e4bda
Ghasem67 <81895501+Ghasem67@users.noreply.github.com>
2022-04-26 11:56:25 +0430

    Add entities layer insfractruture layer migration layer

---
+++ src/Hospital.Migrations/20220426_Init.cs
@@ -0,0 +1,56 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Migrations
+{
+    [Migration(20220426)]
+    public class _20220426_Init : Migration
+    {
+        public override void Up()
+        {
+            CreateAppointment();
+            CreatePatient();
+            CreateDoctor();
+        }
+        public override void Down()
+        {
+            Delete.Table("Appointments");
+            Delete.Table("Patients");
+            Delete.Table("Doctors");
+        }
+        private void CreateAppointment()
+        {
+            Create.Table("Appointments")
+                            .WithColumn("Id").AsInt32().Identity().PrimaryKey()
+                            .WithColumn("Date").AsDate()
+                            .WithColumn("DoctorId").AsInt32().NotNullable()
+                            .ForeignKey("FK_Doctors_Appointments", "Doctors", "Id")
+                            .WithColumn("PatientId").AsInt32().NotNullable()
+                            .ForeignKey("FK_Patients_Appointments", "Patients", "Id");
+        }
+
+        private void CreatePatient()
+        {
+            Create.Table("Patients")
+                             .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                             .WithColumn("FirstName").AsString(50).NotNullable()
+                             .WithColumn("LastName").AsString(50).NotNullable()
+                             .WithColumn("NationalCode").AsString(10).NotNullable();
+        }
+
+        private void CreateDoctor()
+        {
+            Create.Table("Doctors")
+                             .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                             .WithColumn("FirstName").AsString(50).NotNullable()
+                             .WithColumn("LastName").AsString(50).NotNullable()
+                             .WithColumn("NationalCode").AsString(10).NotNullable()
+                             .WithColumn("Field").AsString(50).NotNullable();
+        }
+
+    }
+}
